5e8ecac2c8e8056fc78dd363e2e75ba5cb1ccbce
Aelita Styles <aelita@styles.pm>
2016-04-28 15:07:44 +0100

    Add test for multiple host seperators in user masks

--- parse_test.go
+++ parse_test.go
@@ -463,3 +463,25 @@ func TestParseUserMaskInvalid2(t *testing.T) {
 			"permitted.\", got \"%s\"", err)
 	}
 }
+func TestParseUserMaskInvalid3(t *testing.T) {
+	unparsedmask := "test!x@client@client.test"
+	var parsedmask IrcUserMask
+	var err error
+
+	parsedmask, err = ParseUserMask(unparsedmask)
+	t.Logf("Returned error is: %+v", err)
+	if err == nil {
+		t.Fatalf("ParseUserMask should have failed an with error '%s'", err)
+	}
+
+	if parsedmask.Type != "" || parsedmask.Nick != "" || parsedmask.Username !=
+		"" || parsedmask.Host != "" {
+		t.Fatalf("ParseUserMark returned a non-empty IrcUserMask after an " +
+			"error.")
+	}
+
+	if err.Error() != "User mask has multiple host seperators." {
+		t.Fatalf("Expected error to be \"User mask has multiple host "+
+			"seperators.\", got \"%s\"", err)
+	}
+}
