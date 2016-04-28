1729edf45166e13395f349b343a88798c013a683
Aelita Styles <aelita@styles.pm>
2016-04-28 15:09:03 +0100

    Add test for user masks with dots in the username

--- parse_test.go
+++ parse_test.go
@@ -485,3 +485,25 @@ func TestParseUserMaskInvalid3(t *testing.T) {
 			"seperators.\", got \"%s\"", err)
 	}
 }
+func TestParseUserMaskInvalid4(t *testing.T) {
+	unparsedmask := "test!x.boo@client.test"
+	var parsedmask IrcUserMask
+	var err error
+
+	parsedmask, err = ParseUserMask(unparsedmask)
+	t.Logf("Returned error is: %+v", err)
+	if err == nil {
+		t.Fatalf("ParseUserMask should have failed an with error '%s'", err)
+	}
+
+	if parsedmask.Type != "" || parsedmask.Nick != "" || parsedmask.Username !=
+		"" || parsedmask.Host != "" {
+		t.Fatalf("ParseUserMark returned a non-empty IrcUserMask after an " +
+			"error.")
+	}
+
+	if err.Error() != "Username contains dots, which isn't permitted." {
+		t.Fatalf("Expected error to be \"Username contains dots, which isn't "+
+			"permitted.\", got \"%s\"", err)
+	}
+}
